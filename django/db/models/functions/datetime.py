0c7ca647a11a4c23277f4767be851aacaf157474
Daniel Araújo Miranda <miranda.dam@pf.gov.br>
2022-11-10 19:32:11 -0300

    backport for CVE-2022-34265

--- django/db/models/functions/datetime.py
+++ django/db/models/functions/datetime.py
@@ -41,6 +41,8 @@ class Extract(TimezoneMixin, Transform):
         super().__init__(expression, **extra)
 
     def as_sql(self, compiler, connection):
+        if not connection.ops.extract_trunc_lookup_pattern.fullmatch(self.lookup_name):
+            raise ValueError("Invalid lookup_name: %s" % self.lookup_name)
         sql, params = compiler.compile(self.lhs)
         lhs_output_field = self.lhs.output_field
         if isinstance(lhs_output_field, DateTimeField):
@@ -175,6 +177,8 @@ class TruncBase(TimezoneMixin, Transform):
         super().__init__(expression, output_field=output_field, **extra)
 
     def as_sql(self, compiler, connection):
+        if not connection.ops.extract_trunc_lookup_pattern.fullmatch(self.kind):
+            raise ValueError("Invalid kind: %s" % self.kind)
         inner_sql, inner_params = compiler.compile(self.lhs)
         if isinstance(self.output_field, DateTimeField):
             tzname = self.get_tzname()
